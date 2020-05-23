89d0897059b6146b471f6cca2f131ed99a68bed0
jvictorfarias <victorfarias.new@gmail.com>
2020-05-23 18:31:27 -0300

    :recycle: typeorm models

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -11,13 +11,19 @@ import {
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+@Entity('orders_products')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Order, order => order.order_products)
   order: Order;
 
+  @ManyToOne(() => Product, product => product.order_products)
   product: Product;
 
+  @Column()
+  @JoinColumn()
   product_id: string;
 
   order_id: string;
